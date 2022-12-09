103850f327b5ee94c46a898dcc663b0dcfc13b3a
DoggySummer <91588779+DoggySummer@users.noreply.github.com>
2022-12-09 14:41:09 +0900

    set basic

---
+++ library/src/component/Header.js
@@ -0,0 +1,18 @@
+function Header(){
+  return (
+    <div className="header">
+      <div className="logo">
+        로고위치
+      </div>
+      <div className="headline_list">
+        <div>about</div>
+        <div>매장안내</div>
+        <div>about</div>
+        <div>about</div>
+
+      </div>
+    </div>
+  )
+};
+
+export default Header;
---
+++ library/src/component/Top_img.js
@@ -0,0 +1,14 @@
+function Top_img(){
+  return (
+    <div className="top_background">
+      <div className="dark_side"></div>
+      <img className="background_img" src="img/clear_img.jpg" width="100%" />
+      <div className="background_title">
+        <h1>고품격 스터디 카페</h1>
+        <h3></h3>
+      </div>
+    </div>
+  )
+};
+
+export default Top_img;
