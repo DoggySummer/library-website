19a906176f3f659066b92b1443c1eb34addb8f4f
DoggySummer <91588779+DoggySummer@users.noreply.github.com>
2022-12-10 18:40:18 +0900

    add picture

--- library/src/component/Header.js
+++ library/src/component/Header.js
@@ -1,16 +1,12 @@
 function Header(){
   return (
     <div className="header">
-      <div className="logo">
-        로고위치
-      </div>
-      <div className="headline_list">
-        <div>about</div>
-        <div>매장안내</div>
-        <div>about</div>
-        <div>about</div>
-
-      </div>
+      <img className="logo" src="img/study_logo.png" width="100%" />
+      <ul className="headline_list">
+        <li>about</li>
+        <li>매장안내</li>
+        <li>연락처</li>
+      </ul>
     </div>
   )
 };
--- library/src/component/Top_img.js
+++ library/src/component/Top_img.js
@@ -4,8 +4,9 @@ function Top_img(){
       <div className="dark_side"></div>
       <img className="background_img" src="img/clear_img.jpg" width="100%" />
       <div className="background_title">
-        <h1>고품격 스터디 카페</h1>
-        <h3></h3>
+        <h3>고품격 스터디 카페</h3>
+        <h1>OO스터디 카페</h1>
+        <h3>당신의 꿈이 실현되는 장소</h3>
       </div>
     </div>
   )
